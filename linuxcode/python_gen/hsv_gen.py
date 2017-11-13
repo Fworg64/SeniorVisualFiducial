a6a2f07745b373e383968c1d2af807de86a49793
Marc Olberding <dashneptune@gmail.com>
2017-11-12 20:47:48 -0600

    got rid of bugs with red color binning as well as formatting erros with printing to lookup table

--- linuxcode/python_gen/hsv_gen.py
+++ linuxcode/python_gen/hsv_gen.py
@@ -2,18 +2,14 @@
 
 import sys, os, math
 
-minRed = 
+minRed =250
 maxRed = 14 
 minGreen = 50
-maxGreen = 100
-minBlue = 100
-maxBlue = 300
+maxGreen = 150
+minBlue = 150
+maxBlue = 220
 minSat = 9
 minVal = 3
-
-
-
-
 
 def getrgbtrip (inputbyte):
   r = (0xF800 & inputbyte) >> 11
@@ -64,22 +60,22 @@ def check_code (r,g,b):
   print ('g equals ' + g)
   print ('b equals ' + b)
 
-def print_list_to_lookup (file_ref,lookup, name):
+def print_list_to_lookup (file_ref,lookup, name, entries_per_line):
   lookup_size = len(lookup)
   size_const_name = name.upper() + '_SIZE'
   size_def_str = '#define ' + size_const_name + ' ' + str(lookup_size)
   headerfile.write('\n \n \n')
   headerfile.write(size_def_str + ' \n')
   headerfile.write('\n \n \n')
-  headerfile.write('unsigned int hsv_val[' + size_const_name + '] = { \n')
+  headerfile.write('unsigned int ' +name+ ' [' + size_const_name + '] = { \n')
 
   for x in range(lookup_size):
     headerfile.write(str(lookup[x]))
     if (x != (lookup_size-1)):
       headerfile.write(',')
     else:
-      headerfile.write('} + \n')
-    if (((x % 16)==0) and x != 0):
+      headerfile.write('};' +' \n')
+    if (((x % entries_per_line)==0) and x != 0):
       headerfile.write('\n')
 
 def is_red (hsv_trip):
@@ -90,7 +86,7 @@ def is_red (hsv_trip):
   global minRed
   global maxRed
   global minVal
-  if (hsv_trip[0] < maxRed and hsv_trip[1] > minRed and s > minSat and v > minVal):
+  if ((h < maxRed or h > minRed) and s > minSat and v > minVal):
     return 1
   else:
     return 0
@@ -103,7 +99,7 @@ def is_green (hsv_trip):
   global minGreen
   global maxGreen
   global minVal
-  if (hsv_trip[0] < maxGreen and hsv_trip[1] > minGreen and s > minSat and v > minVal):
+  if (h < maxGreen and h > minGreen and s > minSat and v > minVal):
     return 1
   else:
     return 0
@@ -116,7 +112,7 @@ def is_blue (hsv_trip):
   global minBlue
   global maxBlue
   global minVal
-  if (hsv_trip[0] < maxBlue and hsv_trip[1] > minBlue and s > minSat and v > minVal):
+  if (h < maxBlue and h > minBlue and s > minSat and v > minVal):
     return 1
   else:
     return 0
@@ -144,8 +140,10 @@ hsv_green = [is_green(x) for x in hsv_trips]
 hsv_blue = [is_blue(x) for x in hsv_trips]
 
 
-print_list_to_lookup (headerfile, hsv_packed, 'hsv_lookup')
-
+print_list_to_lookup (headerfile, hsv_packed, 'hsv_lookup', 24)
+print_list_to_lookup (headerfile, hsv_red, 'red_lookup', 60)
+print_list_to_lookup (headerfile, hsv_blue, 'blue_lookup',60)
+print_list_to_lookup (headerfile, hsv_green, 'green_lookup', 60)
 
 headerfile.write(header_guard_end)
 headerfile.close ()
